1dc797d0153e9b8607a5f65238a59e15d8ba935b
nlw0 <nwerneck@gmail.com>
2015-07-13 12:12:37 -0300

    solved finishing sequence

--- src/main/scala/AkkaStreamTest.scala
+++ src/main/scala/AkkaStreamTest.scala
@@ -1,6 +1,6 @@
 import java.io.File
 
-import akka.actor.{Actor, Props}
+import akka.actor.{ActorRef, Actor, Props}
 import akka.stream.ActorMaterializer
 import akka.stream.actor.ActorSubscriberMessage.{OnComplete, OnError, OnNext}
 import akka.stream.actor.{ActorSubscriber, WatermarkRequestStrategy}
@@ -9,7 +9,8 @@ import akka.stream.scaladsl._
 import akka.util.ByteString
 
 import scala.concurrent.ExecutionContext.Implicits.global
-
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class AkkaStreamTest extends Actor {
 
@@ -20,9 +21,7 @@ class AkkaStreamTest extends Actor {
   // read lines from a log file
   val logFile = new File("/home/nic/machado.txt")
 
-
   val stopWords = scala.io.Source.fromFile("src/main/resources/pt_stopwords.txt").getLines().map(_.trim).toSet
-
 
   import akka.stream.io.Implicits._
 
@@ -37,19 +36,24 @@ class AkkaStreamTest extends Actor {
   val res = Source.synchronousFile(logFile)
     .via(Framing.delimiter(ByteString(System.lineSeparator), maximumFrameLength = 8192, allowTruncation = true))
     .map(_.utf8String)
-    .drop(50000).take(1000)
     .mapConcat(ss => getWords(ss).toVector)
-    .to(Sink.actorSubscriber(Props(classOf[WordCountReducer]))).run()
+    .to(Sink.actorSubscriber(Props(classOf[WordCountReducer], self))).run()
 
-  res onSuccess { case _ => context.system.terminate() }
+  res onSuccess { case _ => println("akkabou") }
 
   def receive = {
-    case _ =>
+    case AkkaStreamTest.Hammerdown =>
+      println("END THIS")
+      context.system.terminate()
   }
 }
 
+object AkkaStreamTest {
+  case object Hammerdown
+}
 
-class WordCountReducer extends ActorSubscriber {
+
+class WordCountReducer(supervisor: ActorRef) extends ActorSubscriber {
 
   override val requestStrategy = new WatermarkRequestStrategy(6)
 
@@ -61,12 +65,13 @@ class WordCountReducer extends ActorSubscriber {
 
     case OnError(err: Exception) =>
       println("ERROR: " + err)
-//      context stop self
+      context stop self
 
     case OnComplete =>
       println("FINISHED")
       PrintWordcountResults(count)
-  //    context stop self
+      context.system.scheduler.scheduleOnce(1 second, supervisor, AkkaStreamTest.Hammerdown)
+      context stop self
 
     case xx => println("wtf:" + xx)
   }
